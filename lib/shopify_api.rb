a0ded32ac713b830ab4722ee286403651bc571d8
arosenk35 <anthony@teamlaunch.com>
2019-10-31 20:09:56 -0400

    disable any

--- lib/shopify_api.rb
+++ lib/shopify_api.rb
@@ -264,7 +264,7 @@ class ShopifyAPI
     type=objs_name.split('_')[0]
     sleep(0.3)
   while current_count == page_limit do
-    if objs_name.start_with?('orders')  then params.merge!(:status=>'any') end
+    #if objs_name.start_with?('orders')  then params.merge!(:status=>'any') end
     shopify_objs = api_get objs_name, params.merge(:limit=>page_limit,:page=>current_page)
 
     if type.include?('/')
